9fd1da85131e651db32164f9cb6ec004eb6dc7ee
Lang <157680569+LaangYB@users.noreply.github.com>
2024-09-20 08:19:01 +0700

    Create vars.py

---
+++ LaangYB/vars.py
@@ -0,0 +1,48 @@
+from fipper.types import Message
+
+from pyLaang import CMD_HELP
+from pyLaang.dB.variable import del_var, get_var, set_var
+from pyLaang.decorator import Laang
+from . import yins
+
+@Laang(["set_var"], langs=True)
+async def setdv_handler(client, m: Message, _):
+    cmd = m.command[1] if len(m.command) > 1 else None
+    value = m.text.split(None, 2)[2] if len(m.text.split(None, 2)) > 2 else None
+    if not cmd or not value:
+        return await m.reply(_["vars_1"])
+    else:
+        await set_var(cmd, value)
+        return await m.reply(_["vars_2"].format(cmd, value))
+
+@Laang(["del_var"], langs=True)
+async def deldv_handler(client, m: Message, _):
+    cmd = yins.get_cmd(m)
+    if not cmd:
+        return await m.reply(_["vars_3"])
+    else:
+        await del_var(cmd)
+        return await m.reply(_["vars_4"].format(cmd))
+
+@Laang(["get_var"], langs=True)
+async def getdv_handler(client, m: Message, _):
+    cmd = yins.get_cmd(m)
+    if not cmd:
+        return await m.reply(_["vars_5"])
+    else:
+        done = await get_var(cmd)
+        if done:
+            return await m.reply(_["vars_6"].format(cmd, done))
+        else:
+            return await m.reply(_["vars_7"])
+
+CMD_HELP.update(
+    {"vars": (
+        "vars",
+        {
+            "set_var [variable, value]": "Set vars database.",
+            "del_var [variable]": "Untuk menghapus vars database.",
+            "get_var [variable]": "Untuk mendapatkan vars database.",
+        }
+    )}
+)
