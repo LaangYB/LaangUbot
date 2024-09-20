c8337211263fa58f445e413170ef5ec303e7c5a5
Lang <157680569+LaangYB@users.noreply.github.com>
2024-09-20 08:01:07 +0700

    Create __init__.py

---
+++ assistant/__init__.py
@@ -0,0 +1,32 @@
+# Laang - Ubot
+# Copyright (C) 2024 @LaangYB
+#
+# This file is a part of < https://github.com/LaangYB/LaangUbot >
+# Please read the GNU Affero General Public License in
+# <https://www.github.com/LaangYB/LaangUbot/blob/main/LICENSE/>.
+#
+# FROM LaangUbot <https://github.com/LaangYB/LaangUbot>
+# t.me/ybtraviss & t.me/ybtraviss
+
+
+# ========================×========================
+#            Jangan Hapus Credit Ngentod
+# ========================×========================
+
+import logging
+from config import *
+from git import Repo
+from pyLaang import PyrogramYB
+from pyLaang.Clients import *
+from pyLaang.config import Var
+
+# Mendapatkan repository git dan branch aktif
+repo = Repo()
+branch = repo.active_branch
+
+# Inisialisasi PyrogramYB dan Var
+yins = PyrogramYB()
+var = Var()
+
+# Logger untuk modul ini
+logs = logging.getLogger(__name__)
