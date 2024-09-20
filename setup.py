a4d76bfac6c5e55d1dfab66db49f3b049b0c5a02
Lang <157680569+LaangYB@users.noreply.github.com>
2024-09-20 08:24:01 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nama_paket_anda',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Daftar dependensi Anda
+    ],
+)
